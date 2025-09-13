c804154021acff2db048a827cdd9007aeda5a830
bakriahmadridhwan <bahridhwan@gmail.com>
2025-09-13 16:50:09 +0700

    setup project: setup dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { DarkmodeToggle } from "@/components/common/darkmode-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <DarkmodeToggle />
+      <Button>Button</Button>
     </div>
   );
 }
