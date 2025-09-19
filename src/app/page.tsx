d129b7ef765686cd325a79c97f1e2970eb76fdb1
bakriahmadridhwan <bahridhwan@gmail.com>
2025-09-20 02:27:20 +0700

    dashboard: sidebar menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import { DarkmodeToggle } from "@/components/common/darkmode-toggle";
-import { Button } from "@/components/ui/button";
+import { DarkmodeToggle } from '@/components/common/darkmode-toggle';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-      <DarkmodeToggle />
-      <Button>Button</Button>
+    <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
+      <h1 className="text-4xl font-semibold">Welcome Admin</h1>
+      <Link href="/admin">
+        <Button className="bg-teal-500 text-white hover:bg-teal-700">Access Dashboard</Button>
+      </Link>
     </div>
   );
 }
