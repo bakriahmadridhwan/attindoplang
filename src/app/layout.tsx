4c64a7a4aeaf911303eb7e157b6bedebf4adc3e7
bakriahmadridhwan <bahridhwan@gmail.com>
2025-10-25 00:27:49 +0700

    konfigurasi analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
-import { ThemeProvider } from '@/providers/theme-provider';
-import { Toaster } from '@/components/ui/sonner';
-import AuthStoreProvider from '@/providers/auth-store-provider';
-import { cookies } from 'next/headers';
-import ReactQueryProvider from '@/providers/react-query-provider';
-import { BannerComponent } from './(pages)/(home)/_components/banner';
-import NavRelative from './(pages)/(home)/_components/nav';
-import { FooterComponent } from './(pages)/(home)/_components/footer';
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import AuthStoreProvider from "@/providers/auth-store-provider";
+import { cookies } from "next/headers";
+import ReactQueryProvider from "@/providers/react-query-provider";
+import { BannerComponent } from "./(pages)/(home)/_components/banner";
+import NavRelative from "./(pages)/(home)/_components/nav";
+import { FooterComponent } from "./(pages)/(home)/_components/footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export default async function RootLayout({
@@ -25,7 +26,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const cookiesStore = await cookies();
-  const profile = JSON.parse(cookiesStore.get('user_profile')?.value ?? '{}');
+  const profile = JSON.parse(cookiesStore.get("user_profile")?.value ?? "{}");
   return (
     <html lang="en" suppressHydrationWarning>
       <body
@@ -40,9 +41,10 @@ export default async function RootLayout({
               disableTransitionOnChange
             >
               <BannerComponent />
-              <div className='relative w-full'>
+              <div className="relative w-full">
                 <NavRelative />
                 {children}
+                <Analytics />
                 <Toaster />
               </div>
               <FooterComponent />
